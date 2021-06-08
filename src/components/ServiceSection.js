9dfe81abd920bead07a21bebfb0bfa8589ffd73e
favas <favasizmuofficial@gmail.com>
2021-06-08 20:32:34 +0530

    6. styling service section

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -5,52 +5,82 @@ import diaphragm from "../img/diaphragm.svg";
 import teamWork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
 
+//styles
+import { About, Description, Image } from "../styles";
+import styled from "styled-components";
 //
 const ServiceSection = () => {
   return (
-    <div>
-      <div className="services">
-        <div className="description">
-          <h2>
-            High <span>Quality</span>Services{" "}
-          </h2>
-          <div className="cards">
-            <div className="card">
-              <div className="icon">
-                <img alt="icon" src={clock} />
-                <h3>Efficient</h3>
-              </div>
-              <p>Lorem ipsum dolor sit amet.</p>
+    <Services>
+      <Description>
+        <h2>
+          High <span>Quality</span> Services{" "}
+        </h2>
+        <Cards>
+          <Card>
+            <div className="icon">
+              <img alt="icon" src={clock} />
+              <h3>Efficient</h3>
             </div>
-            <div className="card">
-              <div className="icon">
-                <img alt="icon" src={money} />
-                <h3>Cost-Effective</h3>
-              </div>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img alt="icon" src={money} />
+              <h3>Cost-Effective</h3>
             </div>
-            <div className="card">
-              <div className="icon">
-                <img alt="icon" src={teamWork} />
-                <h3>TeamWork</h3>
-              </div>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img alt="icon" src={teamWork} />
+              <h3>TeamWork</h3>
             </div>
-            <div className="card">
-              <div className="icon">
-                <img alt="icon" src={diaphragm} />
-                <h3>Diaphragm</h3>
-              </div>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img alt="icon" src={diaphragm} />
+              <h3>Diaphragm</h3>
             </div>
-          </div>
-        </div>
-        <div className="image">
-          <img alt="Homescreen2" src={home2} />
-        </div>
-      </div>
-    </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
+        <img alt="Homescreen2" src={home2} />
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+  }
+  h3 {
+    margin-left: 1rem;
+    background: white;
+    color: black;
+    padding: 1rem;
+  }
+`;
 
 export default ServiceSection;
