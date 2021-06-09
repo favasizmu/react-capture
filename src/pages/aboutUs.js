d436458665a04e9c831e3ea4395655a62f1ca17f
favas <favasizmuofficial@gmail.com>
2021-06-09 11:54:31 +0530

    modified

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -5,10 +5,12 @@ import React from "react";
 import AboutSection from "../components/aboutSection";
 import ServiceSection from "../components/ServiceSection";
 import FaqSection from "../components/FaqSection";
+import NavSection from "../components/nav";
 
 const aboutUs = () => {
   return (
     <>
+      <NavSection />
       <AboutSection />
       <ServiceSection />
       <FaqSection />
