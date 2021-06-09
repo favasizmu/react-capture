4c0427e075b28c6c228493270c8d19f43de4163e
favas <favasizmuofficial@gmail.com>
2021-06-09 08:31:56 +0530

    7.faq styling

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import styled from "styled-components";
+import { About } from "../styles";
+
 const faq = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions? <span>FAQ</span>
       </h2>
@@ -15,6 +18,7 @@ const faq = () => {
             provident.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="questions">
         <h4>How Do I Pay</h4>
@@ -25,6 +29,7 @@ const faq = () => {
             provident.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="questions">
         <h4>How do I Call</h4>
@@ -35,6 +40,7 @@ const faq = () => {
             provident.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="questions">
         <h4>Timing</h4>
@@ -45,9 +51,37 @@ const faq = () => {
             provident.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 00rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default faq;
